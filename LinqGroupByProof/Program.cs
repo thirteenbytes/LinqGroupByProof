cb10c1d446f4f6e906f49a81f45beb05d9d450bc
thirteenbytes <pdragun@13bytes.com>
2024-12-02 21:00:35 -0500

    replicated error - working on addin gmore data to write quick solution.

--- LinqGroupByProof/Program.cs
+++ LinqGroupByProof/Program.cs
@@ -13,7 +13,7 @@ var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
         // Get the connection string from appsettings.json
-        var connectionString = context.Configuration.GetConnectionString("Main");
+        var connectionString = context.Configuration.GetConnectionString("Current");
 
         // Register DbContext with the connection string
         services.AddDbContext<ApplicationDbContext>(options =>
@@ -25,44 +25,88 @@ var host = Host.CreateDefaultBuilder(args)
 using var scope = host.Services.CreateScope();
 var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-Console.WriteLine("Ensuring database is created...");
-dbContext.Database.EnsureCreated();
-Console.WriteLine("Database created!");
+//Console.WriteLine("Ensuring database is created...");
+//dbContext.Database.EnsureCreated();
+//Console.WriteLine("Database created!");
 
 
-//var memberPhotos = new List<MemberPhoto>
-//{
-//    new MemberPhoto 
-//    { 
-//        Id = Guid.NewGuid(), 
-//        DateTaken = DateTime.Now, 
-//        PhotoType = PhotoType.Member, 
-//        Status = PhotoStatus.Approved 
-//    }
-//};
+List<MemberAdded> memberAddeds = new List<MemberAdded>
+{
+    new MemberAdded("John Henry", MemberRole.Student),
+    new MemberAdded("Roaxanne Smith", MemberRole.Student),
+    new MemberAdded("Simone Juryvouski", MemberRole.Student),
+    new MemberAdded("James Smith", MemberRole.Student),
+    new MemberAdded("Hector Mac", MemberRole.Student),
+    new MemberAdded("Phil Smith", MemberRole.Student),
+    new MemberAdded("Jane Doe", MemberRole.Staff),
+    new MemberAdded("Bob Henry", MemberRole.Staff),
+    new MemberAdded("Cecil Peters", MemberRole.Staff),
+};
 
-//dbContext.Set<Member>().Add(
-//    new Member 
-//    { 
-//        Id = Guid.NewGuid(), 
-//        Name = "John Doe", 
-//        Role = MemberRole.Student, 
-//        MemberPhotos = memberPhotos,        
-//    });
 
-//dbContext.SaveChanges();
+var memberPhotos = new List<MemberPhoto>
+{
+    new MemberPhoto
+    {
+        Id = Guid.NewGuid(),
+        DateTaken = DateTime.Now,
+        PhotoType = PhotoType.Member,
+        Status = PhotoStatus.Approved
+    }
+};
 
-//var memberPhoto = dbContext.Set<MemberPhoto>().First();
-//var member = dbContext.Set<Member>().First();
+foreach (var memberToAdd in memberAddeds)
+{
+    dbContext.Set<Member>().Add(
+        new Member
+        {
+            Id = Guid.NewGuid(),
+            Name = memberToAdd.name,
+            Role = memberToAdd.role,
+            MemberPhotos = memberPhotos,
+        });
+}
 
-//member.ActiveMemberPhoto = memberPhoto;
+dbContext.SaveChanges();
 
-//dbContext.Set<Member>().Update(member);
-//dbContext.SaveChanges();
+var memberPhoto = dbContext.Set<MemberPhoto>().First();
+var member = dbContext.Set<Member>().First();
 
+member.ActiveMemberPhoto = memberPhoto;
+
+dbContext.Set<Member>().Update(member);
+dbContext.SaveChanges();
+
+record MemberAdded(string name, MemberRole role);
 
 //var result = dbContext.Set<Member>()
 //    .Select(m => new
 //    {
-
+//        m.Id,
+//        m.Role,
+//        MemberPhoto = m.ActiveMemberPhoto != null ? m.ActiveMemberPhoto : 
+//        m.MemberPhotos
+//        .OrderBy(mp =>
+//            mp.Status == PhotoStatus.Approved ? 0 : 
+//            mp.Status == PhotoStatus.Rejected ? 1 : 2)
+//        .ThenByDescending(mp => mp.DateTaken)
+//        .FirstOrDefault()
 //    })
+//    .Select(m=> new
+//    {
+//        m.Id,
+//        m.Role,
+//        MemberPhotoStatus = m.MemberPhoto != null ? m.MemberPhoto.Status : PhotoStatus.NotPresent
+//    })
+//    .GroupBy(g=> new {g.Role, g.MemberPhotoStatus})
+//    .Select(g => new
+//    {
+//        g.Key.Role,
+//        g.Key.MemberPhotoStatus,
+//        Count = g.Count()
+//    });
+
+//Console.WriteLine(result.ToQueryString());
+
+
+
